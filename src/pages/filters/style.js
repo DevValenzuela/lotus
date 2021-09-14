380b338d2e06aeae5baf5dd6673103e8ba4e6795
Wdark <wdark@Develops-MacBook-Air.local>
2021-09-14 16:55:55 -0500

    Update Lotus

---
+++ src/pages/filters/style.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+export const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#330066',
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  dateTitle: {
+    color: '#fff',
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#660066',
+    marginVertical: 5,
+    marginHorizontal: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  image: {
+    width: 25,
+    height: 25,
+    marginRight: 10,
+  },
+});
