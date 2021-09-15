7abda0817943ba25f4b88ed5e3d0f1e2c8b9d566
valenzuela21 <vlzdavid12@outlook.com>
2021-09-14 22:46:26 -0500

    Update Lotus

--- android/app/src/main/java/com/lotus/MainActivity.java
+++ android/app/src/main/java/com/lotus/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lotus;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -9,7 +10,14 @@ public class MainActivity extends ReactActivity {
    * rendering of the component.
    */
   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
+
+  @Override
   protected String getMainComponentName() {
     return "lotus";
   }
+
 }
--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {RouterNavigation} from './router.navigation';
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
   return (
     <>
       <NavigationContainer>
