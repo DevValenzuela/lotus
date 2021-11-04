6086bae62dca933a6ae08cd56177d0e18b6e240a
Wdark <wdark@Develops-MacBook-Air.local>
2021-11-04 16:37:21 -0500

    Update Lotus

--- src/pages/filters/Vaccinate.js
+++ src/pages/filters/Vaccinate.js
@@ -7,23 +7,65 @@ import {
   View,
   Image,
   TextInput,
+  TouchableHighlight,
 } from 'react-native';
 import {style} from './style';
 import {useDebounceValue} from '../../hooks/debounceTime';
 import {CONSULT_SEARCH_FILTER_VACCINATIONS} from '../apolllo/query';
 import {useIsConnected} from 'react-native-offline';
 import {database2} from '../../conexion/crudSqlite2';
+import {useNavigation} from '@react-navigation/native';
 
-const Item = ({date}) => (
-  <View style={style.item}>
-    <Image
-      source={require('./../../assets/images/tabs/VACCINEICON.png')}
-      resizeMode="contain"
-      style={style.image}
-    />
-    <Text style={style.dateTitle}>{date}</Text>
-  </View>
-);
+const Item = ({data}) => {
+  const {date, id, id_vaccination} = data;
+  const navigation = useNavigation();
+  return (
+    <View
+      style={[
+        style.item,
+        {
+          flexDirection: 'row',
+        },
+      ]}>
+      <View style={{flex: 1}}>
+        <Image
+          source={require('./../../assets/images/tabs/VACCINEICON.png')}
+          resizeMode="contain"
+          style={style.image}
+        />
+      </View>
+      <View style={{flex: 3}}>
+        <Text style={style.dateTitle}>{date}</Text>
+      </View>
+      <View style={{flex: 1}}>
+        <TouchableHighlight
+          style={{alignItems: 'center'}}
+          onPress={() =>
+            navigation.navigate('DetailsGeneral', {
+              idDetails: id_vaccination,
+              type: 'vacunacion',
+            })
+          }
+          underlayColor="transparent">
+          <View
+            style={{
+              paddingHorizontal: 14,
+              paddingVertical: 0,
+              backgroundColor: 'rgba(51,0,102,0.56)',
+              margin: 4,
+              borderRadius: 4,
+            }}>
+            <Image
+              source={require('../../assets/images/detailsicon.png')}
+              resizeMode="contain"
+              style={style.iconActions}
+            />
+          </View>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
 
 const VaccinateFilters = () => {
   const [txtValue, setTxtValue] = useState('');
@@ -36,7 +78,7 @@ const VaccinateFilters = () => {
     CONSULT_SEARCH_FILTER_VACCINATIONS,
   );
 
-  const renderItem = ({item}) => <Item date={item.date} />;
+  const renderItem = ({item}) => <Item data={item} />;
 
   useEffect(() => {
     if (value && isConnected) {
@@ -44,6 +86,7 @@ const VaccinateFilters = () => {
       value.vacunacions.map(item => {
         result.push({
           id: item.id,
+          id_vaccination: item.id_vaccination,
           date: item.last_vaccination,
         });
       });
