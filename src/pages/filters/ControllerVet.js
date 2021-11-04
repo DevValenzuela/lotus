44388c5c804c37cb233b28335dafad7e1b24f5d8
Wdark <wdark@Develops-MacBook-Air.local>
2021-11-04 16:43:45 -0500

    Update Lotus

--- src/pages/filters/ControllerVet.js
+++ src/pages/filters/ControllerVet.js
@@ -7,30 +7,72 @@ import {
   View,
   Image,
   TextInput,
+  TouchableHighlight,
 } from 'react-native';
 import {style} from './style';
 import {useDebounceValue} from '../../hooks/debounceTime';
 import {CONSULT_SEARCH_FILTER_DOCTOR} from '../apolllo/query';
 import {useIsConnected} from 'react-native-offline';
 import {database2} from '../../conexion/crudSqlite2';
+import {useNavigation} from '@react-navigation/native';
 
-const Item = ({date}) => (
-  <View style={style.item}>
-    <Image
-      source={require('./../../assets/images/tabs/DOCTORICON.png')}
-      resizeMode="contain"
-      style={style.image}
-    />
-    <Text style={style.dateTitle}>{date}</Text>
-  </View>
-);
+const Item = ({data}) => {
+  const {date, id, id_medic} = data;
+  const navigation = useNavigation();
+  return (
+    <View
+      style={[
+        style.item,
+        {
+          flexDirection: 'row',
+        },
+      ]}>
+      <View style={{flex: 1}}>
+        <Image
+          source={require('./../../assets/images/tabs/DOCTORICON.png')}
+          resizeMode="contain"
+          style={style.image}
+        />
+      </View>
+      <View style={{flex: 3}}>
+        <Text style={style.dateTitle}>{date}</Text>
+      </View>
+      <View style={{flex: 1}}>
+        <TouchableHighlight
+          style={{alignItems: 'center'}}
+          onPress={() =>
+            navigation.navigate('DetailsGeneral', {
+              idDetails: id_medic,
+              type: 'control medico',
+            })
+          }
+          underlayColor="transparent">
+          <View
+            style={{
+              paddingHorizontal: 14,
+              paddingVertical: 0,
+              backgroundColor: 'rgba(51,0,102,0.56)',
+              margin: 4,
+              borderRadius: 4,
+            }}>
+            <Image
+              source={require('../../assets/images/detailsicon.png')}
+              resizeMode="contain"
+              style={style.iconActions}
+            />
+          </View>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
 
 const ControllerVet = () => {
   const [txtValue, setTxtValue] = useState('');
   const [getSearchResult, setSearchResult] = useState([]);
   const isConnected = useIsConnected();
   const value = useDebounceValue(txtValue, 1000, CONSULT_SEARCH_FILTER_DOCTOR);
-  const renderItem = ({item}) => <Item date={item.date} />;
+  const renderItem = ({item}) => <Item data={item} />;
 
   useEffect(() => {
     if (value && isConnected) {
@@ -38,6 +80,7 @@ const ControllerVet = () => {
       value.controllerMedics.map(item => {
         result.push({
           id: item.id,
+          id_medic: item.id_medic,
           date: item.last_control,
         });
       });
